e02894e49a9e8e7be9fe880eadfee8e1dcbf6012
Michael D. Schlueb <mschlueb19@gmail.com>
2026-05-13 19:11:15 -0400

    Changing the logout link

--- src/App.jsx
+++ src/App.jsx
@@ -16,22 +16,11 @@ function LandingPage() {
   const auth = useOIDCAuth();
   //console.log("Auth state in LandingPage:", auth);
 
-  const signOutRedirect = async () => {
-    try {
-      // 1. Tell Amplify to clear local tokens and storage
-      await signOut(); 
-    
-      // 2. Define your variables
-      const { userPoolClientId, loginWith } = amplifyconfig.Auth.Cognito;
-      const { domain, redirectSignOut } = loginWith.oauth;
-    
-      // 3. Now send them to the Hosted UI to clear the server session
-      const cognitoUrl = `https://${domain}/logout?client_id=${userPoolClientId}&logout_uri=${encodeURIComponent(redirectSignOut[0])}`;
-    
-      window.location.href = cognitoUrl;
-    } catch (error) {
-      console.error('Error during sign out:', error);
-    }
+  const signOutRedirect = () => {
+    const clientId = "5d32h4mt57n9ljti8d8fhkcflt";
+    const logoutUri = "https://main.dymkwrcw8goz2.amplifyapp.com/";
+    const cognitoDomain = "https://us-east-25krmfc4ny.auth.us-east-2.amazoncognito.com";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
   };
 
   if (auth.isLoading) {
