3e7c360a6ea4172cde8d8b90b31ad3b9146aedec
Michael D. Schlueb <mschlueb19@gmail.com>
2026-05-14 18:17:09 -0400

    Messed up with the authentication stage

--- src/App.jsx
+++ src/App.jsx
@@ -28,11 +28,6 @@ function LandingPage() {
   };
 
   if (auth.isLoading) {
-    // Add this logic where your Auth confirms the user is logged in
-    if (window.location.search.includes("code=")) {
-      // This removes the ?code=... from the address bar without reloading the page
-      window.history.replaceState({}, document.title, window.location.pathname);
-    }
     return <div>Loading...</div>;
   }
 
