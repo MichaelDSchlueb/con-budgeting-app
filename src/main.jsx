a05b0746367000c4e64e2622e4cbc5f088a2ec2e
Michael D. Schlueb <mschlueb19@gmail.com>
2026-05-14 18:23:42 -0400

    Trying from Main

--- src/main.jsx
+++ src/main.jsx
@@ -38,9 +38,16 @@ const amplifyconfig = {
 Amplify.configure(amplifyconfig);
 
 
+
 // Use it in your render
 
 console.log("Starting app render...");
+
+// Add this logic where your Auth confirms the user is logged in
+if (window.location.search.includes("code=")) {
+    // This removes the ?code=... from the address bar without reloading the page
+    window.history.replaceState({}, document.title, window.location.pathname);
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
