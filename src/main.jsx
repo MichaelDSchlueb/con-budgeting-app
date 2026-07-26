d9ba6e3f3383b9fdd560340cbe9d5fcc861f7aa7
Michael D. Schlueb <mschlueb19@gmail.com>
2026-07-26 18:51:30 -0400

    Fixing authentication only here so we can begin with testing on the deletion.

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import { AuthProvider } from "react-oidc-context";
 import { CookieStorage } from 'aws-amplify/utils';
 
 // main.jsx
+
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_5KrMfc4NY",
   client_id: "5d32h4mt57n9ljti8d8fhkcflt",
@@ -16,6 +17,8 @@ const cognitoAuthConfig = {
   response_type: "code",
   scope: "email openid phone",
 };
+
+const currentOrigin = window.location.origin;
 
 // 2. Use the EXACT same name in the configure block
 const amplifyconfig = {
@@ -27,8 +30,14 @@ const amplifyconfig = {
         oauth: {
           domain: 'your-cognito-domain.auth.us-east-2.amazoncognito.com',
           scopes: ['openid', 'email', 'profile', 'aws.cognito.signin.user.admin'],
-          redirectSignIn: ['https://main.dymkwrcw8goz2.amplifyapp.com/'],
-          redirectSignOut: ['https://main.dymkwrcw8goz2.amplifyapp.com/'],
+          redirectSignIn: [
+            `${currentOrigin}/`,
+            'https://main.dymkwrcw8goz2.amplifyapp.com/'
+          ],
+          redirectSignOut: [
+            `${currentOrigin}/`,
+            'https://main.dymkwrcw8goz2.amplifyapp.com/'
+          ],
           responseType: 'code'
         }
       }
